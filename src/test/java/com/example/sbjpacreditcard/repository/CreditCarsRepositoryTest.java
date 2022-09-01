adde63fdd9cb8c03f63bc12c42cfd60520a9103c
PavelBlinets <pavelblinets@gmail.com>
2022-09-01 20:09:04 +0300

    simple test

---
+++ src/test/java/com/example/sbjpacreditcard/repository/CreditCarsRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.sbjpacreditcard.repository;
+
+import com.example.sbjpacreditcard.domain.CreditCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//@DataJpaTest
+@SpringBootTest
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditCarsRepositoryTest {
+
+    private final String CREDIT_CARD_NUMBER = "1234567898765";
+
+    @Autowired
+    private CreditCarsRepository creditCarsRepository;
+
+    @Test
+    void workCreditCarsRepositoryTest() {
+        CreditCard creditCard = new CreditCard();
+        creditCard.setCreditCardNumber(CREDIT_CARD_NUMBER);
+        creditCard.setCvv("800");
+        creditCard.setExpirationDate("12/2030");
+
+        CreditCard save = creditCarsRepository.saveAndFlush(creditCard);
+        CreditCard creditCard1 = creditCarsRepository.findById(save.getId()).get();
+        assertThat(creditCard1.getCreditCardNumber()).isEqualTo(CREDIT_CARD_NUMBER);
+
+    }
+}
