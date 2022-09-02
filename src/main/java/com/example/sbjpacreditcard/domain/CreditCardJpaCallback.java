0892932034986b8ac4fa74278bf7b5a53cf07269
PavelBlinets <pavelblinets@gmail.com>
2022-09-02 10:48:42 +0300

    impl JPA callback and ApplicationContextAware

--- src/main/java/com/example/sbjpacreditcard/domain/CreditCardJpaCallback.java
+++ src/main/java/com/example/sbjpacreditcard/domain/CreditCardJpaCallback.java
@@ -1,19 +1,27 @@
 package com.example.sbjpacreditcard.domain;
 
+import com.example.sbjpacreditcard.config.SpringContextHelper;
+import com.example.sbjpacreditcard.services.EncryptionService;
 import jakarta.persistence.*;
 
 public class CreditCardJpaCallback {
 
     @PrePersist
     @PreUpdate
-    public void beforeInsertOrUpdate(CreditCard creditCard){
+    public void beforeInsertOrUpdate(CreditCard creditCard) {
         System.out.println("before Insert Or Update");
+        creditCard.setCreditCardNumber(getEncryptionService().encrypt(creditCard.getCreditCardNumber()));
     }
 
     @PostLoad
     @PostUpdate
     @PostPersist
-    public void postLoad(CreditCard creditCard){
+    public void postLoad(CreditCard creditCard) {
         System.out.println("post Load");
+        creditCard.setCreditCardNumber(getEncryptionService().decrypt(creditCard.getCreditCardNumber()));
+    }
+
+    private EncryptionService getEncryptionService() {
+        return SpringContextHelper.getApplicationContext().getBean(EncryptionService.class);
     }
 }
